315e3a45a157ef50faedea3ca1ef4453154f3ad4
keroken <kenichi.shinagawa@gmail.com>
2021-04-05 13:53:52 +0900

    add axis and normalize chats

--- src/Test03.tsx
+++ src/Test03.tsx
@@ -3,7 +3,7 @@ import { select, Selection } from 'd3-selection'
 import { axisBottom, axisLeft, max, scaleBand, scaleLinear } from 'd3';
 import styled from 'styled-components'
 
-const data = [
+const initialData = [
   {
     name: 'foo',
     number: 9000
@@ -31,24 +31,27 @@ const dimensions = {
   height: 500,
   chartWidth: 700,
   chartHeight: 400,
+  marginTop: 50,
   marginLeft: 100
 }
 
 const Test03: React.FC = () => {
   const ref = useRef<SVGSVGElement | null>(null)
   const [selection, setSelection] = useState<null | Selection<SVGSVGElement | null, unknown, null, undefined>>(null)
+  const [data, setData] = useState(initialData)
 
   const maxValue = max(data, d => d.number)
   const y = scaleLinear()
       .domain([0, maxValue!])
-      .range([0, dimensions.chartHeight])
+      .range([dimensions.chartHeight, 0])
 
   const x = scaleBand()
       .domain(data.map(d => d.name))
       .range([0, dimensions.chartWidth])
-      .paddingInner(0.3)
+      .paddingInner(0.1)
 
   const yAxis = axisLeft(y)
+          .ticks(3)
   const xAxis = axisBottom(x)
 
   useEffect(() => {
@@ -57,16 +60,16 @@ const Test03: React.FC = () => {
     } else {
       const xAxisGroup = selection
         .append('g')
-        .attr('transform', `translate(${dimensions.marginLeft}, ${dimensions.chartHeight})`)
+        .attr('transform', `translate(${dimensions.marginLeft}, ${dimensions.chartHeight + dimensions.marginTop})`)
         .call(xAxis)
       const yAxisGroup = selection
         .append('g')
-        .attr('transform', `translate(${dimensions.marginLeft}, 0)`)
+        .attr('transform', `translate(${dimensions.marginLeft}, ${dimensions.marginTop})`)
         .call(yAxis)
 
       selection
         .append('g')
-        .attr('transform', `translate(${dimensions.marginLeft},0)`)
+        .attr('transform', `translate(${dimensions.marginLeft}, ${dimensions.marginTop})`)
         .selectAll('rect')
         .data(data)
         .enter()
@@ -79,10 +82,11 @@ const Test03: React.FC = () => {
           }
           return null
         })
+        .attr('y', d => y(d.number))
         .attr('fill', 'orange')
-        .attr('height', d => y(d.number))
+        .attr('height', d => dimensions.chartHeight  - y(d.number))
     }
-  }, [selection, x, xAxis, y, yAxis])
+  }, [data, selection, x, xAxis, y, yAxis])
 
   return (
     <StyledBase>

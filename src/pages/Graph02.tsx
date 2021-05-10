3d2b0b9f4d44d543c9edd0efb3eccae6ea34c6ef
keroken <kenichi.shinagawa@gmail.com>
2021-05-10 19:04:11 +0900

    update keys

--- src/pages/Graph02.tsx
+++ src/pages/Graph02.tsx
@@ -47,10 +47,10 @@ const Graph02: React.FC = () => {
   const horizontalLines = [];
   for (let i = 0; i < 5; i++) {
     horizontalLines.push(
-      <>
+      <g key={i}>
         <line x1="0" y1={i * 40} x2={dimensions.chartWidth} y2={i * 40} stroke="#E9EAEB" />
         <text x={dimensions.chartWidth + 10} dy="5" y={i * 40} fontSize={12} fill="#7C868A">{`${4 - i}0分`}</text>
-      </>
+      </g>
     );
   }
 

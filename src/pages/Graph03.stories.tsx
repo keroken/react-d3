db1ea25222eafebf2d5712651d423ec9ef0a74b2
keroken <kenichi.shinagawa@gmail.com>
2021-05-25 21:27:12 +0900

    make Graph03 stories

--- src/pages/Graph03.stories.tsx
+++ src/pages/Graph03.stories.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react';
-
 import Graph03 from './Graph03';
 
 export default {
@@ -9,12 +7,6 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as Meta;
-
-const Template: Story = (args) => <Graph03 {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Graph03',
 };
+
+export const Primary = () => <Graph03 />;

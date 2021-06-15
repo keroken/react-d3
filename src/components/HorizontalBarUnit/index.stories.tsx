7acf73e624823c01e71a6c84e944beef71459073
keroken <kenichi.shinagawa@gmail.com>
2021-06-15 13:24:01 +0900

    change bar color

--- src/components/HorizontalBarUnit/index.stories.tsx
+++ src/components/HorizontalBarUnit/index.stories.tsx
@@ -15,7 +15,7 @@ export const Primary = () => {
   return (
     <svg>
       <g transform="translate(50, 50)">
-        <HorizontalBarUnit unitValue={data} graphRatio={96} color="#8AE58B" />
+        <HorizontalBarUnit unitValue={data} graphRatio={96} color="#17D4E5" />
       </g>
     </svg>
   );

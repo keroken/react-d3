752e5dd8d65f1e8cadfb1cff25635532c7948e46
keroken <kenichi.shinagawa@gmail.com>
2021-06-16 22:51:17 +0900

    adjust bar unit story

--- src/components/HorizontalBarUnit/index.stories.tsx
+++ src/components/HorizontalBarUnit/index.stories.tsx
@@ -13,11 +13,7 @@ export default makeMeta({
 });
 export const Primary = () => {
   return (
-    <svg>
-      <g transform="translate(50, 50)">
-        <HorizontalBarUnit unitValue={data} graphRatio={96} color="#17D4E5" />
-      </g>
-    </svg>
+    <HorizontalBarUnit unitValue={data} graphRatio={96} color="#17D4E5" />
   );
 };
 

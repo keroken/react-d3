3d50f8c713274c92bdb9f0d81a6bef4c1d4774ab
keroken <kenichi.shinagawa@gmail.com>
2021-06-26 23:55:50 +0900

    add styles/color and typography

---
+++ src/styles/Typography.ts
@@ -0,0 +1,54 @@
+import { css } from 'styled-components';
+
+export const Tokens = {
+  Heading1: css`
+    font-size: 18px;
+    font-weight: normal;
+    line-height: 24px;
+  `,
+  Heading2: css`
+    font-size: 16px;
+    font-weight: normal;
+    line-height: 20px;
+  `,
+  Heading3: css`
+    font-size: 13px;
+    font-weight: bold;
+    line-height: 16px;
+  `,
+  Body1: css`
+    font-size: 13px;
+    font-weight: normal;
+    line-height: 18px;
+  `,
+  Body2: css`
+    font-size: 12px;
+    font-weight: normal;
+    line-height: 16px;
+  `,
+  Label1: css`
+    font-size: 14px;
+    font-weight: bold;
+    line-height: 14px;
+  `,
+  Label2: css`
+    font-size: 14px;
+    font-weight: normal;
+    line-height: 14px;
+  `,
+  Label3: css`
+    font-size: 12px;
+    font-weight: bold;
+    line-height: 12px;
+  `,
+  Label4: css`
+    font-size: 12px;
+    font-weight: normal;
+    line-height: 12px;
+  `,
+  Label5: css`
+    font-size: 11px;
+    font-weight: normal;
+    line-height: 11px;
+  `,
+} as const;

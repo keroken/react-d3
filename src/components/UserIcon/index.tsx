f857def78650b810eebfb318d741faff16bbcb13
keroken <kenichi.shinagawa@gmail.com>
2021-07-05 20:10:54 +0900

    UserIcon - add show data option

--- src/components/UserIcon/index.tsx
+++ src/components/UserIcon/index.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 type Props = {
   imageUrl: string;
   size: 'small' | 'regular' | 'large';
-  isActive: boolean;
+  isActive?: boolean;
+  showData?: boolean;
+  dataValue?: number;
 };
 
-export const UserIcon = ({ imageUrl, size, isActive }: Props) => {
+export const UserIcon = ({ imageUrl, size, isActive, showData, dataValue = 0 }: Props) => {
+  const radius = 16;
+  const innerRadius = 14.5;
+  const circumference = 2 * Math.PI * innerRadius;
+  const [offset, setOffset] = useState(0);
+  useEffect(() => {
+    const progressOffset = -(dataValue / 100) * circumference;
+    setOffset(progressOffset);
+  }, [setOffset, circumference, dataValue, offset]);
+
   return (
     <>
       <StyledMemberIcon imageUrl={imageUrl} size={size}>
         {isActive && <StyledBadge size={size} />}
+        {showData && (
+          <StyledSVG>
+            <StyledCircle fill="transparent" stroke="#17D4E5" strokeWidth={3} cx={radius} cy={radius} r={innerRadius} />
+            <StyledCircle
+              fill="transparent"
+              stroke="#DEDFE0"
+              strokeWidth={3}
+              cx={radius}
+              cy={radius}
+              r={innerRadius}
+              strokeDasharray={circumference}
+              strokeDashoffset={offset}
+            />
+          </StyledSVG>
+        )}
       </StyledMemberIcon>
     </>
   );
@@ -67,4 +93,12 @@ const StyledBadge = styled.span<Pick<Props, 'size'>>`
   border-radius: 999px;
   border: 1px solid white;
   background-color: #17D4E5;
+`;
+
+const StyledSVG = styled.svg`
+  transform: rotate(-90deg);
+`;
+
+const StyledCircle = styled.circle`
+  transition: stroke-dashoffset 600ms ease-in-out;
 `;

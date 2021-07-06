a3bb26af164f991c98618f9e448f7222b0440a54
keroken <kenichi.shinagawa@gmail.com>
2021-07-06 22:43:26 +0900

    UserIcon - update props and add initial option

--- src/components/UserIcon/index.tsx
+++ src/components/UserIcon/index.tsx
@@ -2,26 +2,30 @@ import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 type Props = {
-  imageUrl: string;
+  name: string;
+  bgColor: string;
+  imageUrl?: string;
   size: 'small' | 'regular' | 'large';
   isActive?: boolean;
   showData?: boolean;
   dataValue?: number;
 };
 
-export const UserIcon = ({ imageUrl, size, isActive, showData, dataValue = 0 }: Props) => {
-  const radius = 16;
-  const innerRadius = 14.5;
+export const UserIcon = ({ name, bgColor, imageUrl, size, isActive, showData, dataValue = 0 }: Props) => {
+  const radius = size === 'large' ? 32 : size === 'regular' ? 16 : 12;
+  const innerRadius = size === 'large' ? 30.5 : size === 'regular' ? 14.5 : 10.5;
   const circumference = 2 * Math.PI * innerRadius;
   const [offset, setOffset] = useState(0);
   useEffect(() => {
     const progressOffset = -(dataValue / 100) * circumference;
     setOffset(progressOffset);
   }, [setOffset, circumference, dataValue, offset]);
+  const initial = name.slice(0, 1);
 
   return (
     <>
-      <StyledMemberIcon imageUrl={imageUrl} size={size}>
+      <StyledMemberIcon imageUrl={imageUrl} size={size} bgColor={bgColor}>
+        {imageUrl ? null : initial}
         {isActive && <StyledBadge size={size} />}
         {showData && (
           <StyledSVG>
@@ -43,32 +47,43 @@ export const UserIcon = ({ imageUrl, size, isActive, showData, dataValue = 0 }: 
   );
 };
 
-const StyledMemberIcon = styled.span<Pick<Props, 'imageUrl' | 'size'>>`
+const StyledMemberIcon = styled.span<Pick<Props, 'imageUrl' | 'size' | 'bgColor'>>`
   position: relative;
   box-sizing: border-box;
   display: flex;
+  align-items: center;
+  justify-content: center;
   ${({ size }) => {
     switch (size) {
       case 'small':
         return css`
           width: 24px;
           height: 24px;
+          font-size: 14px;
         `;
       case 'large':
         return css`
           width: 64px;
           height: 64px;
+          font-size: 24px;
         `;
       default:
         return css`
           width: 32px;
           height: 32px;
+          font-size: 16px;
         `;
     }
   }};
-  background-image: url(${props => props.imageUrl});
-  background-position: center;
-  background-size: cover;
+  color: #202428;
+  background-color: ${props => props.bgColor};
+  ${props =>
+    props.imageUrl &&
+    css`
+      background-image: url(${props.imageUrl});
+      background-position: center;
+      background-size: cover;
+    `}
   border-radius: 999px;
 `;
 
@@ -96,6 +111,9 @@ const StyledBadge = styled.span<Pick<Props, 'size'>>`
 `;
 
 const StyledSVG = styled.svg`
+  position: absolute;
+  width: 100%;
+  height: 100%;
   transform: rotate(-90deg);
 `;
 

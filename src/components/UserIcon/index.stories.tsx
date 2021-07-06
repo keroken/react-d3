7bb0c30ea11cced8b98f9ae6bf44880850a63c19
keroken <kenichi.shinagawa@gmail.com>
2021-07-06 22:43:40 +0900

    UserIcon - update story

--- src/components/UserIcon/index.stories.tsx
+++ src/components/UserIcon/index.stories.tsx
@@ -1,7 +1,8 @@
 import { UserIcon } from '.';
-import { boolean, number, select } from '@storybook/addon-knobs';
+import { boolean, number } from '@storybook/addon-knobs';
 import { makeMeta } from '../../helpers/Story';
 import React from 'react';
+import styled from 'styled-components';
 
 export default makeMeta({
   component: UserIcon,
@@ -14,18 +15,64 @@ export default makeMeta({
 });
 
 export const Basic = () => (
-  <UserIcon
-    imageUrl="images/interview-image01.png"
-    size={select('size', ['small', 'regular', 'large'], 'regular')}
-    isActive={boolean('isActive', true)}
-  />
+  <StyledContainer>
+    <UserIcon
+      name="笹野 健二"
+      bgColor="YellowGreen"
+      imageUrl="images/interview-image01.png"
+      size="small"
+      isActive={boolean('isActive', true)}
+    />
+    <UserIcon
+      name="笹野 健二"
+      bgColor="YellowGreen"
+      imageUrl="images/interview-image01.png"
+      size="regular"
+      isActive={boolean('isActive', true)}
+    />
+    <UserIcon
+      name="笹野 健二"
+      bgColor="YellowGreen"
+      imageUrl="images/interview-image01.png"
+      size="large"
+      isActive={boolean('isActive', true)}
+    />
+  </StyledContainer>
 );
 
 export const WithData = () => (
-  <UserIcon
-    imageUrl="images/interview-image01.png"
-    size={select('size', ['small', 'regular', 'large'], 'regular')}
-    showData
-    dataValue={number('data value', 75)}
-  />
+  <StyledContainer>
+    <UserIcon
+      name="笹野 健二"
+      bgColor="YellowGreen"
+      imageUrl="images/interview-image01.png"
+      size="small"
+      showData={boolean('show data', true)}
+      dataValue={number('data value', 75)}
+    />
+    <UserIcon
+      name="笹野 健二"
+      bgColor="YellowGreen"
+      imageUrl="images/interview-image01.png"
+      size="regular"
+      showData={boolean('show data', true)}
+      dataValue={number('data value', 75)}
+    />
+    <UserIcon
+      name="笹野 健二"
+      bgColor="YellowGreen"
+      imageUrl="images/interview-image01.png"
+      size="large"
+      showData={boolean('show data', true)}
+      dataValue={number('data value', 75)}
+    />
+  </StyledContainer>
 );
+
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: center;
+  & > span {
+    margin-right: 16px;
+  }
+`;

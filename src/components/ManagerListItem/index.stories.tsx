436a426ef4bc2b9cc58b8187e950d20eb452aa66
keroken <kenichi.shinagawa@gmail.com>
2021-07-13 22:57:57 +0900

    add ManagerListItem

---
+++ src/components/ManagerListItem/index.stories.tsx
@@ -0,0 +1,122 @@
+import { ManagerListItem } from '.';
+import { makeMeta } from '@/helpers/Story';
+import { userDataType } from '@/components/userDataType';
+import React from 'react';
+import styled from 'styled-components';
+
+export default makeMeta({
+  component: ManagerListItem,
+  meta: {
+    package: 'core',
+    type: 'components',
+    category: 'layout',
+    name: 'ManagerListItem',
+  },
+});
+export const Basic = () => {
+  return (
+    <StyledTable>
+      <ManagerListItem userData={userData} memberData={memberData} />
+    </StyledTable>
+  );
+};
+
+const StyledTable = styled.table`
+  width: 100%;
+`;
+
+const userData: userDataType = {
+  id: 1,
+  name: '笹野 健二',
+  userColor: 'Blue',
+  role: 'リーダー',
+  team: 'カスタマーサクセス',
+  sessionCount: 10,
+  lastSessionDate: '2021-06-15',
+  imageUrl: 'images/interview-image01.png',
+  value_a: 63,
+  value_b: 42,
+  isActive: true,
+};
+
+const memberData: userDataType[] = [
+  {
+    id: 1,
+    name: '君枝 芳子',
+    userColor: 'Pink',
+    role: 'サブリーダー',
+    team: 'カスタマーサクセス',
+    sessionCount: 5,
+    lastSessionDate: '2021-06-20',
+    imageUrl: 'images/interview-image02.png',
+    value_a: 67,
+    value_b: 30,
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: '加藤 真一',
+    userColor: 'Yellow',
+    role: 'サブリーダー',
+    team: 'カスタマーサクセス',
+    sessionCount: 5,
+    lastSessionDate: '2021-06-20',
+    imageUrl: undefined,
+    value_a: 88,
+    value_b: 30,
+    isActive: true,
+  },
+  {
+    id: 3,
+    name: '山本 彩香',
+    userColor: 'Purple',
+    role: 'メンバー',
+    team: 'カスタマーサクセス',
+    sessionCount: 5,
+    lastSessionDate: '2021-06-20',
+    imageUrl: undefined,
+    value_a: 92,
+    value_b: 30,
+    isActive: true,
+  },
+  {
+    id: 4,
+    name: '久保 浩子',
+    userColor: 'Blue',
+    role: 'メンバー',
+    team: 'カスタマーサクセス',
+    sessionCount: 5,
+    lastSessionDate: '2021-06-20',
+    imageUrl: undefined,
+    value_a: 43,
+    value_b: 30,
+    isActive: true,
+  },
+  {
+    id: 5,
+    name: '長谷川 佳美',
+    nameForIcon: '長谷川',
+    userColor: 'Aquamarine',
+    role: 'メンバー',
+    team: 'カスタマーサクセス',
+    sessionCount: 5,
+    lastSessionDate: '2021-06-20',
+    imageUrl: undefined,
+    value_a: 92,
+    value_b: 30,
+    isActive: true,
+  },
+  {
+    id: 6,
+    name: '渋谷 圭介',
+    userColor: 'Orange',
+    role: 'メンバー',
+    team: 'カスタマーサクセス',
+    sessionCount: 5,
+    lastSessionDate: '2021-06-20',
+    imageUrl: undefined,
+    value_a: 43,
+    value_b: 30,
+    isActive: true,
+  },
+];
---
+++ src/components/ManagerListItem/index.tsx
@@ -0,0 +1,146 @@
+import { BorderRadius, ColorTokens, TypographyTokens, space } from '@/styles';
+import { Icon } from '@/components/Icon';
+import { LiveIconButton } from '@/components/LiveIconButton';
+import { UserIcon } from '@/components/UserIcon';
+import { format } from 'date-fns';
+import { userDataType } from '@/components/userDataType';
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  userData: userDataType;
+  memberData: userDataType[];
+};
+
+export const ManagerListItem = ({ userData, memberData }: Props) => {
+  return (
+    <StyledTableRow>
+      <StyledUserCell>
+        <UserIcon
+          name={userData.name}
+          nameForIcon={userData.nameForIcon}
+          bgColor="YellowGreen"
+          imageUrl={userData.imageUrl}
+          isActive={userData.isActive}
+          size="regular"
+        />
+        <StyledNameContainer>
+          <StyledMemberName>{userData.name}</StyledMemberName>
+          <StyledTeamName>{userData.team}</StyledTeamName>
+        </StyledNameContainer>
+      </StyledUserCell>
+      <StyledIconGroupCell>
+        {memberData.map((member, index) =>
+          index < 4 ? (
+            <UserIcon
+              key={member.id}
+              name={member.name}
+              nameForIcon={member.nameForIcon}
+              size="regular"
+              bgColor={member.userColor}
+              imageUrl={member.imageUrl}
+              showData
+              dataValue={member.value_a}
+            />
+          ) : index === 4 ? (
+            <StyledMoreIcon name="more" size="Large" />
+          ) : null,
+        )}
+      </StyledIconGroupCell>
+      <td>
+        <StyledSessionCount>{userData.sessionCount}回</StyledSessionCount>
+      </td>
+      <td>
+        <StyledLastSessionDate>{format(new Date(userData.lastSessionDate), "MM'月'dd'日'")}</StyledLastSessionDate>
+      </td>
+      <StyledIconButtonContainer>
+        <LiveIconButton icon="more" size="regular" styling="light" id="more" />
+      </StyledIconButtonContainer>
+    </StyledTableRow>
+  );
+};
+
+const StyledTableRow = styled.tr`
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 64px;
+  padding: 0 ${space(2)};
+  border-top: 1px solid lightGray;
+  border-bottom: 1px solid lightGray;
+`;
+
+const StyledUserCell = styled.td`
+  display: flex;
+  align-items: flex-start;
+  min-width: 200px;
+  padding: ${space(3)} 0 ${space(2)};
+`;
+
+const StyledNameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: ${space(2)};
+`;
+
+const StyledMemberName = styled.span`
+  display: flex;
+  align-items: center;
+  width: 120px;
+  margin-bottom: ${space(1)};
+  ${TypographyTokens.Label3};
+`;
+
+const StyledTeamName = styled.span`
+  ${TypographyTokens.Label6};
+  color: ${ColorTokens.Text02};
+`;
+
+const StyledIconGroupCell = styled.td`
+  display: flex;
+  align-items: center;
+  min-width: 220px;
+  & > span {
+    margin-right: ${space(1)};
+  }
+`;
+
+const StyledSessionCount = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 40px;
+  margin-right: ${space(16)};
+  ${TypographyTokens.Label4};
+  color: ${ColorTokens.Text02};
+  text-align: right;
+`;
+
+const StyledLastSessionDate = styled.div`
+  display: flex;
+  align-items: center;
+  width: 60px;
+  margin-right: ${space(16)};
+  ${TypographyTokens.Label4};
+  color: ${ColorTokens.Text02};
+  text-align: right;
+`;
+
+const StyledIconButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  & button:first-of-type {
+    margin-right: ${space(4)};
+  }
+`;
+
+const StyledMoreIcon = styled(Icon)`
+  & {
+    background-color: ${ColorTokens.Ui01};
+    border-radius: ${BorderRadius.Circle};
+  }
+`;

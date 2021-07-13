60c91a6bea77011283be1ba0a6ca1d40fc89c6da
keroken <kenichi.shinagawa@gmail.com>
2021-07-13 22:42:06 +0900

    fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,19 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    '@storybook/addon-knobs',
+    '@storybook/addon-actions', 
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async config => {
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+    config.resolve.alias['helpers'] = path.resolve(__dirname, '../src/helpers');
+    return config;
+  },
 }

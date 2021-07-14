0cd1484718b899aa322b0f090d7c31c70a25dad7
keroken <kenichi.shinagawa@gmail.com>
2021-07-14 19:30:27 +0900

    MemberListItem - update story

--- src/components/MemberListItem/index.stories.tsx
+++ src/components/MemberListItem/index.stories.tsx
@@ -1,5 +1,6 @@
-import { MemberListItem, dataType } from '.';
+import { MemberListItem } from '.';
 import { makeMeta } from '../../helpers/Story';
+import { userDataType } from '@/components/userDataType';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -24,10 +25,16 @@ const StyledTable = styled.table`
   width: 100%;
 `;
 
-const memberData: dataType = {
+const memberData: userDataType = {
   id: 1,
-  name: '笹野賢二',
-  imageUrl: 'images/interview-image02.png',
+  name: '笹野 健二',
+  userColor: 'Blue',
+  role: 'リーダー',
+  team: 'カスタマーサクセス',
+  sessionCount: 10,
+  lastSessionDate: '2021-06-15',
+  imageUrl: 'images/interview-image01.png',
   value_a: 63,
   value_b: 42,
+  isActive: true,
 };

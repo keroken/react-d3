cbec6bb5cbbf22f805a07ddfe5efb0d6999f9139
keroken <kenichi.shinagawa@gmail.com>
2021-07-15 22:06:51 +0900

    update UserButton

--- src/components/UserButton/index.stories.tsx
+++ src/components/UserButton/index.stories.tsx
@@ -1,5 +1,6 @@
 import { UserButton } from '.';
 import { makeMeta } from '../../helpers/Story';
+import { userDataType } from '@/components/userDataType';
 import React from 'react';
 
 export default makeMeta({
@@ -12,11 +13,33 @@ export default makeMeta({
   },
 });
 export const Basic = () => {
-  return <UserButton name={data.name} imageUrl={data.imageUrl} isActive={data.isActive} />;
+  return (
+    <UserButton
+      id={data.id}
+      role={data.role}
+      name={data.name}
+      userColor={data.userColor}
+      imageUrl={data.imageUrl}
+      isActive={data.isActive}
+      team={data.team}
+      value_a={data.value_a}
+      value_b={data.value_b}
+      sessionCount={data.sessionCount}
+      lastSessionDate={data.lastSessionDate}
+    />
+  );
 };
 
-const data = {
-  name: '松井 麻衣',
+const data: userDataType = {
+  id: 1,
+  name: '久保 浩子',
+  userColor: 'Yellow',
+  role: 'リーダー',
+  team: 'カスタマーサクセス',
+  sessionCount: 10,
+  lastSessionDate: '2021-06-15',
   imageUrl: 'images/interview-image02.png',
+  value_a: 63,
+  value_b: 42,
   isActive: true,
 };

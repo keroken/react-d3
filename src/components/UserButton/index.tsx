07d53cccdc6926f69c8a425f73cbda58df90be1a
keroken <kenichi.shinagawa@gmail.com>
2021-07-15 22:07:07 +0900

    update UserButton story

--- src/components/UserButton/index.tsx
+++ src/components/UserButton/index.tsx
@@ -1,17 +1,13 @@
+import { BorderRadius, ColorPalette, ColorTokens, TypographyTokens, space } from '@/styles';
 import { UserIcon } from '..//UserIcon';
+import { userDataType } from '@/components/userDataType';
 import React from 'react';
 import styled from 'styled-components';
 
-type Props = {
-  name: string;
-  imageUrl: string;
-  isActive: boolean;
-};
-
-export const UserButton = ({ name, imageUrl, isActive }: Props) => {
+export const UserButton = ({ name, userColor, imageUrl, isActive }: userDataType) => {
   return (
     <StyledButton>
-      <UserIcon name={name} bgColor="lightblue" imageUrl={imageUrl} size="small" isActive={isActive} />
+      <UserIcon name={name} bgColor={userColor} imageUrl={imageUrl} size="small" isActive={isActive} />
       <StyledName>{name}</StyledName>
     </StyledButton>
   );
@@ -22,26 +18,24 @@ const StyledButton = styled.button`
   align-items: center;
   width: 100%;
   height: 32px;
-  padding: 4px 8px;
-  font-size: 12px;
-  font-weight: normal;
-  line-height: 12px;
-  color: #7C868A;
+  padding: ${space(1)} ${space(2)};
+  ${TypographyTokens.Label4};
+  color: ${ColorTokens.Text03};
   background-color: transparent;
   border: none;
-  border-radius: 4px;
+  border-radius: ${BorderRadius.Default};
   cursor: pointer;
   &:hover {
-    background-color: #E9EAEB;
+    background-color: ${ColorTokens.HoverUi};
   }
   &:active,
   &:focus {
     font-weight: bold;
-    color: #246BB2;
-    background-color: #B9D8F7;
+    color: ${ColorPalette.Blue70};
+    background-color: ${ColorPalette.Blue30};
   }
 `;
 
 const StyledName = styled.span`
-  margin-left: 8px;
+  margin-left: ${space(2)};
 `;

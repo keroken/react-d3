74b3ae0de42cee326af600bca0e4456fa84fbcc9
keroken <kenichi.shinagawa@gmail.com>
2021-07-18 00:13:19 +0900

    update SurveyModal story

--- src/components/SurveyModal/index.stories.tsx
+++ src/components/SurveyModal/index.stories.tsx
@@ -17,13 +17,23 @@ export const Basic = () => {
   const [isOpen, setIsOpen] = useState(true);
   const handleToggleOpen = useCallback(() => setIsOpen(prevState => !prevState), []);
 
+  const handleSave = () => {
+    alert('saved!')
+    setIsOpen(false);
+    window.location.reload();
+  };
+
+  const handleSkip = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
       <StyledLabel onClick={e => e.stopPropagation()}>
         <input type="checkbox" checked={isOpen} onChange={handleToggleOpen} style={{ margin: '10px' }} />
         open
       </StyledLabel>
-      <SurveyModal isOpen={isOpen} setIsOpen={setIsOpen} questions={questions} />
+      <SurveyModal isOpen={isOpen} setIsOpen={setIsOpen} questions={questions} handleSave={handleSave} handleSkip={handleSkip} after={false} />
     </>
   );
 };
@@ -38,53 +48,96 @@ const StyledLabel = styled.label`
   }
 `;
 
+export type QuestionType = {
+  id: number;
+  survey_id: number;
+  content: string;
+  position: number;
+  selections: Array<SelectionType>;
+};
+
+export type SelectionType = {
+  id: number;
+  question_id: number;
+  content: string;
+  position: number;
+};
+
 const questions = [
   {
     id: 1,
-    text: 'ミスをよく批判されますか？',
-    choices: [
-      { value: 5, label: '非常にそう思う' },
-      { value: 4, label: 'そう思う' },
-      { value: 3, label: 'どちらとも思わない' },
-      { value: 2, label: 'あまりそう思わない' },
-      { value: 1, label: '全くそう思わない' },
-    ],
-    answer: null,
+    survey_id: 1,
+    content: 'ミスをよく批判されますか？',
+    position: 1,
+    selections: [
+      {
+        id: 1,
+        question_id: 1,
+        content: '非常にそう思う',
+        position: 1,
+      },
+      {
+        id: 2,
+        question_id: 2,
+        content: 'そう思う',
+        position: 2,
+      },
+      {
+        id: 3,
+        question_id: 3,
+        content: 'どちらとも思わない',
+        position: 3,
+      },
+      {
+        id: 4,
+        question_id: 4,
+        content: 'あまりそう思わない',
+        position: 4,
+      },
+      {
+        id: 5,
+        question_id: 5,
+        content: '全くそう思わない',
+        position: 5,
+      },
+    ]
   },
   {
     id: 2,
-    text: 'マネージャーとの関係は良好だと思いますか？',
-    choices: [
-      { value: 5, label: '非常にそう思う' },
-      { value: 4, label: 'そう思う' },
-      { value: 3, label: 'どちらとも思わない' },
-      { value: 2, label: 'あまりそう思わない' },
-      { value: 1, label: '全くそう思わない' },
-    ],
-    answer: null,
-  },
-  {
-    id: 3,
-    text: 'ミスをよく批判されますか？',
-    choices: [
-      { value: 5, label: '非常にそう思う非常にそう思うああ' },
-      { value: 4, label: 'そう思う' },
-      { value: 3, label: 'どちらとも思わない' },
-      { value: 2, label: 'あまりそう思わない' },
-      { value: 1, label: '全くそう思わない' },
-    ],
-    answer: null,
-  },
-  {
-    id: 4,
-    text: 'ミスをよく批判されますか？',
-    choices: [
-      { value: 5, label: '非常にそう思う' },
-      { value: 4, label: 'そう思う' },
-      { value: 3, label: 'どちらとも思わない' },
-      { value: 2, label: 'あまりそう思わない' },
-      { value: 1, label: '全くそう思わない' },
-    ],
-    answer: null,
+    survey_id: 2,
+    content: 'マネージャーとの関係は良好だと思いますか？',
+    position: 2,
+    selections: [
+      {
+        id: 1,
+        question_id: 1,
+        content: '非常にそう思う',
+        position: 1,
+      },
+      {
+        id: 2,
+        question_id: 2,
+        content: 'そう思う',
+        position: 2,
+      },
+      {
+        id: 3,
+        question_id: 3,
+        content: 'どちらとも思わない',
+        position: 3,
+      },
+      {
+        id: 4,
+        question_id: 4,
+        content: 'あまりそう思わない',
+        position: 4,
+      },
+      {
+        id: 5,
+        question_id: 5,
+        content: '全くそう思わない',
+        position: 5,
+      },
+    ]
   },
 ];
